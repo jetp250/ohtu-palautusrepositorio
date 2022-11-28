dce488ee67c31d3f857a02c6a9d7623dd6e2628d
jetp250 <jetp250@gmail.com>
2022-11-28 21:05:24 +0200

    (Part 4) Add test to bring coverage to 100%

--- viikko4/verkkokauppa/src/tests/kauppa_test.py
+++ viikko4/verkkokauppa/src/tests/kauppa_test.py
@@ -139,4 +139,83 @@ class TestKauppa(unittest.TestCase):
 
         self.pankki_mock.tilisiirto.assert_called_with("matti", 43, "23456", "33333-44455", 5)
 
-    
+    def test_aloita_asiointi_nollaa_tiedot(self):
+        self.viitegeneraattori_mock.uusi.return_value = 43
+
+        def varasto_saldo(tuote_id):
+            if tuote_id == 1:
+                return 10
+
+        def varasto_hae_tuote(tuote_id):
+            if tuote_id == 1:
+                return Tuote(1, "maito", 5)
+
+        self.varasto_mock.saldo.side_effect = varasto_saldo
+        self.varasto_mock.hae_tuote.side_effect = varasto_hae_tuote
+
+        kauppa = Kauppa(self.varasto_mock, self.pankki_mock, self.viitegeneraattori_mock)
+
+        kauppa.aloita_asiointi()
+        kauppa.lisaa_koriin(1)
+        kauppa.tilimaksu("matti", "23456")
+        self.pankki_mock.tilisiirto.assert_called_with("matti", 43, "23456", "33333-44455", 5)
+
+        kauppa.aloita_asiointi()
+        kauppa.lisaa_koriin(1)
+        kauppa.tilimaksu("matti", "23456")
+        self.pankki_mock.tilisiirto.assert_called_with("matti", 43, "23456", "33333-44455", 5)
+
+    def test_jokaiselle_maksutapahtumalle_generoidaan_viite(self):
+        self.viitegeneraattori_mock.uusi.return_value = 43
+
+        def varasto_saldo(tuote_id):
+            if tuote_id == 1:
+                return 10
+
+        def varasto_hae_tuote(tuote_id):
+            if tuote_id == 1:
+                return Tuote(1, "maito", 5)
+
+        self.varasto_mock.saldo.side_effect = varasto_saldo
+        self.varasto_mock.hae_tuote.side_effect = varasto_hae_tuote
+
+        kauppa = Kauppa(self.varasto_mock, self.pankki_mock, self.viitegeneraattori_mock)
+
+        kauppa.aloita_asiointi()
+        kauppa.lisaa_koriin(1)
+        kauppa.tilimaksu("matti", "23456")
+        self.viitegeneraattori_mock.uusi.assert_called()
+
+        self.viitegeneraattori_mock.uusi.called = False
+
+        kauppa.aloita_asiointi()
+        kauppa.lisaa_koriin(1)
+        kauppa.tilimaksu("matti", "23456")
+        self.viitegeneraattori_mock.uusi.assert_called()
+
+    def test_ostoskorista_poistetusta_tuotteesta_ei_veloiteta(self):
+        self.viitegeneraattori_mock.uusi.return_value = 43
+
+        def varasto_saldo(tuote_id):
+            if tuote_id == 1:
+                return 10
+            if tuote_id == 2:
+                return 5
+
+        def varasto_hae_tuote(tuote_id):
+            if tuote_id == 1:
+                return Tuote(1, "maito", 5)
+            if tuote_id == 2:
+                return Tuote(2, "tomaatti", 2)
+
+        self.varasto_mock.saldo.side_effect = varasto_saldo
+        self.varasto_mock.hae_tuote.side_effect = varasto_hae_tuote
+
+        kauppa = Kauppa(self.varasto_mock, self.pankki_mock, self.viitegeneraattori_mock)
+
+        kauppa.aloita_asiointi()
+        kauppa.lisaa_koriin(1)
+        kauppa.lisaa_koriin(2)
+        kauppa.poista_korista(1)
+        kauppa.tilimaksu("matti", "23456")
+        self.pankki_mock.tilisiirto.assert_called_with("matti", 43, "23456", "33333-44455", 2)
