05de78bea7fcee7d8ea5f7225487e0dbec8e2fd8
jetp250 <jetp250@gmail.com>
2022-11-28 21:45:56 +0200

    (Part 5) #12: adding an item twice results in one item with correct name and qty 2

--- viikko4/tdd-ostoskori/src/tests/ostoskori_test.py
+++ viikko4/tdd-ostoskori/src/tests/ostoskori_test.py
@@ -47,7 +47,6 @@ class TestOstoskori(unittest.TestCase):
 
         self.assertEqual(self.kori.tavaroita_korissa(), 2)
 
-    # Onko liian pitkiä nimiä edes olemassa? Tämähän on täydellisen kuvaava.
     def test_kahden_saman_tuotteen_lisaamisen_jalkeen_korin_hinta_on_kaksi_kertaa_tavaran_hinta(self):
         maito = Tuote("Maito", 3)
         self.kori.lisaa_tuote(maito)
@@ -90,3 +89,12 @@ class TestOstoskori(unittest.TestCase):
 
         ostokset = self.kori.ostokset()
         self.assertEqual(len(ostokset), 1)
+
+    def test_kahden_saman_tuotteen_lisaamisen_jalkeen_korissa_yksi_ostos_jolla_nimi_oikein_ja_maara_kaksi(self):
+        maito = Tuote("Maito", 3)
+        self.kori.lisaa_tuote(maito)
+        self.kori.lisaa_tuote(maito)
+
+        ostos = self.kori.ostokset()[0]
+        self.assertEqual(ostos.tuotteen_nimi(), "Maito")
+        self.assertEqual(ostos.lukumaara(), 2)
