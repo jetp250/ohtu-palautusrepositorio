772b393740381c49b75646f5e142b643f1882725
jetp250 <jetp250@gmail.com>
2022-11-28 21:27:56 +0200

    (Part 5) #7: total price after adding same item twice is 2x the item price

--- viikko4/tdd-ostoskori/src/tests/ostoskori_test.py
+++ viikko4/tdd-ostoskori/src/tests/ostoskori_test.py
@@ -46,3 +46,11 @@ class TestOstoskori(unittest.TestCase):
         self.kori.lisaa_tuote(maito)
 
         self.assertEqual(self.kori.tavaroita_korissa(), 2)
+
+    # Onko liian pitkiä nimiä edes olemassa? Tämähän on täydellisen kuvaava.
+    def test_kahden_saman_tuotteen_lisaamisen_jalkeen_korin_hinta_on_kaksi_kertaa_tavaran_hinta(self):
+        maito = Tuote("Maito", 3)
+        self.kori.lisaa_tuote(maito)
+        self.kori.lisaa_tuote(maito)
+
+        self.assertEqual(self.kori.hinta(), 2 * maito.hinta())
